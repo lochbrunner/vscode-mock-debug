d0189d854d09bfa7f89e43e62b0d4559d0aff173
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2024-03-03 15:39:59 +0000

    Introduce await notify

---
+++ src/orig/awaitNotify.ts
@@ -0,0 +1,44 @@
+class Waiter {
+  timer?: number = undefined;
+
+  constructor(public resolve: () => void) {
+    this.resolve = resolve;
+  }
+
+  do() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.resolve();
+  }
+}
+
+/** A subject that can be awaited on. */
+export class Subject {
+  private readonly waiters: Waiter[] = [];
+
+  async wait(timeout?: number): Promise<void> {
+    const self = this;
+    const promise = new Promise<void>((resolve: () => void) => {
+      const waiter = new Waiter(resolve);
+      if (timeout) {
+        waiter.timer = setTimeout(resolve, timeout);
+      }
+      self.waiters.push(waiter);
+    });
+
+    return promise;
+  }
+
+  notifyAll() {
+    for (const waiter of this.waiters) {
+      waiter.do();
+    }
+  }
+
+  notify() {
+    if (this.waiters.length > 0) {
+      this.waiters.pop().do();
+    }
+  }
+}
